ed1e7600975bb8e78cd35e29dd1d07885fcfdb56
Dawid Gąbka <gabka.daw@gmail.com>
2023-12-12 17:04:51 +0100

    improve error handling in install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -ex
+
 if [ -z "$TV_IP" ]
 then
-  echo "\$TV_IP is empty!"
+  echo "TV_IP environment varaible is empty. Add it or overwrite the container entrypoint."
   exit 1
 fi
-
 
 sdb connect ${TV_IP}
 
 DEVICE_ID=$(sdb devices | grep ${TV_IP} | tr -s ' ' | cut -d ' ' -f 3)
+
+if [ -z "$DEVICE_ID" ]
+then
+  echo "Device ID not found. Perhaps TV_IP is invalid."
+  exit 1
+fi
 
 tizen install -n Jellyfin.wgt -t ${DEVICE_ID}
